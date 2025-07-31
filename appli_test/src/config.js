373d09283e313e61e290117db84c817c51a4574d
Anthony Paboeuf <62709270+Delsus78@users.noreply.github.com>
2025-08-01 01:38:41 +0200

    Update config.js

--- appli_test/src/config.js
+++ appli_test/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 
 export default {
-    //apiUrl: 'https://switstigpty-api.team-unc.fr/'
-    apiUrl: 'https://localhost:7265/',
+    apiUrl: 'https://switstigpty-api.team-unc.fr/'
+    //apiUrl: 'https://localhost:7265/',
 };
